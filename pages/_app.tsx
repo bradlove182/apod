ab14aecee3f48af3b88766ae9130219dcdd0ce2d
Bradley Love <brad@bradlove.co.za>
2022-07-14 16:10:29 +0200

    pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,6 +19,43 @@ const MyApp = ({
                 { "Astronomy Picture of the Day" }
             </title>
             <link href="/favicon.svg" rel="icon" />
+            <meta content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" name="viewport" />
+
+            <meta content="PWA App" name="application-name" />
+            <meta content="yes" name="apple-mobile-web-app-capable" />
+            <meta content="default" name="apple-mobile-web-app-status-bar-style" />
+            <meta content="PWA App" name="apple-mobile-web-app-title" />
+            <meta content="Best PWA App in the world" name="description" />
+            <meta content="telephone=no" name="format-detection" />
+            <meta content="yes" name="mobile-web-app-capable" />
+            <meta content="/icons/browserconfig.xml" name="msapplication-config" />
+            <meta content="#2B5797" name="msapplication-TileColor" />
+            <meta content="no" name="msapplication-tap-highlight" />
+            <meta content="#000000" name="theme-color" />
+
+            <link href="/icons/touch-icon-iphone.png" rel="apple-touch-icon" />
+            <link href="/icons/touch-icon-ipad.png" rel="apple-touch-icon" sizes="152x152" />
+            <link href="/icons/touch-icon-iphone-retina.png" rel="apple-touch-icon" sizes="180x180" />
+            <link href="/icons/touch-icon-ipad-retina.png" rel="apple-touch-icon" sizes="167x167" />
+
+            <link href="/icons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+            <link href="/icons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+            <link href="/manifest.json" rel="manifest" />
+            <link color="#5bbad5" href="/icons/safari-pinned-tab.svg" rel="mask-icon" />
+            <link href="/favicon.ico" rel="shortcut icon" />
+
+            <meta content="summary" name="twitter:card" />
+            <meta content="https://yourdomain.com" name="twitter:url" />
+            <meta content="PWA App" name="twitter:title" />
+            <meta content="Best PWA App in the world" name="twitter:description" />
+            <meta content="https://yourdomain.com/icons/android-chrome-192x192.png" name="twitter:image" />
+            <meta content="@DavidWShadow" name="twitter:creator" />
+            <meta content="website" property="og:type" />
+            <meta content="PWA App" property="og:title" />
+            <meta content="Best PWA App in the world" property="og:description" />
+            <meta content="PWA App" property="og:site_name" />
+            <meta content="https://yourdomain.com" property="og:url" />
+            <meta content="https://yourdomain.com/icons/apple-touch-icon.png" property="og:image" />
         </Head>
         <Navigation />
         <Component { ...pageProps } />
---
+++ pages/_offline.tsx
@@ -0,0 +1,45 @@
+
+import Link from "next/link";
+import React from "react";
+import Head from "next/head";
+
+const Offline = (): JSX.Element => (
+    <React.Fragment>
+        <Head>
+            <title>
+                { "404 - Page Not Found" }
+            </title>
+        </Head>
+        <div className="fixed inset-0 flex justify-center content-center">
+            <div className="grid grid-rows-3 gap-4 h-min self-center prose text-primary">
+                <svg
+                    className="mx-auto hover:animate-spin"
+                    fill="none"
+                    height="64"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    width="64"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <circle cx="12" cy="12" r="10" />
+                    <line x1="8" x2="16" y1="15" y2="15" />
+                    <line x1="9" x2="9.01" y1="9" y2="9" />
+                    <line x1="15" x2="15.01" y1="9" y2="9" />
+                </svg>
+                <h1 className="text-center">
+                    { "You're unfortunately offline. Please reconnect to continue." }
+                </h1>
+                <Link href="/">
+                    <button className="btn" type="button">
+                        { "Home" }
+                    </button>
+                </Link>
+            </div>
+        </div>
+    </React.Fragment>
+);
+
+export default Offline;
