908f44eb173ab3a43f3a328f16f2afe732242388
Bradley Love <brad@bradlove.co.za>
2022-07-16 10:15:07 +0200

    Share Modal, readme

--- components/picture/actions/index.tsx
+++ components/picture/actions/index.tsx
@@ -1,19 +1,14 @@
 
 import React, {
     useCallback,
-    useEffect,
-    useRef,
-    useState
+    useRef
 } from "react";
 
 import {
     DownloadIcon,
-    FacebookIcon,
-    LinkedInIcon,
-    MailIcon,
-    ShareIcon,
-    TwitterIcon
+    ShareIcon
 } from "../../../icons";
+import { ShareModal } from "../share-modal";
 
 import type { Picture } from "../../../data/api/picture/types";
 
@@ -27,7 +22,6 @@ export const PictureActions: React.ComponentType<PictureActionsProps> = ({
 }) => {
 
     const shareToggle = useRef<HTMLInputElement>(null);
-    const [shareLink, setShareLink] = useState<string>("");
 
     const handleShareToggle = useCallback(() => {
 
@@ -38,12 +32,6 @@ export const PictureActions: React.ComponentType<PictureActionsProps> = ({
         }
 
     }, []);
-
-    useEffect(() => {
-
-        setShareLink(window.location.href);
-
-    }, [picture]);
 
     return (
         <React.Fragment>
@@ -59,59 +47,7 @@ export const PictureActions: React.ComponentType<PictureActionsProps> = ({
                     </a>
                 </div>
             </div>
-            <input aria-hidden="true" className="modal-toggle hidden" id="share-modal" ref={ shareToggle } type="checkbox" />
-            <div className="modal modal-bottom sm:modal-middle">
-                <div className="modal-box relative">
-                    <div className="prose">
-                        <h3 className="text-center mb-4">
-                            { "Share" }
-                        </h3>
-                    </div>
-                    <div className="gap-2 flex relative justify-center">
-                        <a
-                            aria-label="Share Facebook"
-                            className="btn btn-square"
-                            href={ `https://www.facebook.com/sharer.php?u=${ shareLink }` }
-                            rel="noreferrer"
-                            target="_blank"
-                        >
-                            <FacebookIcon />
-                        </a>
-                        <a
-                            aria-label="Share Twitter"
-                            className="btn btn-square"
-                            href={ `https://twitter.com/share?url=${ shareLink }&text=${ picture.title }` }
-                            rel="noreferrer"
-                            target="_blank"
-                        >
-                            <TwitterIcon />
-                        </a>
-                        <a
-                            aria-label="Share LinkedIn"
-                            className="btn btn-square"
-                            href={ `https://www.linkedin.com/shareArticle?mini=true&url=${ shareLink }` }
-                            rel="noreferrer"
-                            target="_blank"
-                        >
-                            <LinkedInIcon />
-                        </a>
-                        <a
-                            aria-label="Share Email"
-                            className="btn btn-square"
-                            href={ `mailto:?&subject=Astronomy Picture of the Day: ${ picture.title }&body=${ picture.explanation } - ${ shareLink }` }
-                            rel="noreferrer"
-                            target="_blank"
-                        >
-                            <MailIcon />
-                        </a>
-                    </div>
-                    <div className="modal-action">
-                        <label className="btn btn-ghost btn-block" htmlFor="share-modal">
-                            {"Close"}
-                        </label>
-                    </div>
-                </div>
-            </div>
+            <ShareModal picture={ picture } ref={ shareToggle } />
         </React.Fragment>
     );
 
