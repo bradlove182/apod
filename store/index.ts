951f27b66c771c5287f5a9d84cbcd231158aaf39
Bradley Love <brad@bradlove.co.za>
2023-09-15 15:53:47 +0200

    Update index.ts

--- store/index.ts
+++ store/index.ts
@@ -1,11 +1,10 @@
-
 import { create } from "zustand";
 import { subscribeWithSelector } from "zustand/middleware";
 import { DateTime } from "luxon";
 
 import type { Modals } from "../components/modal";
 
-interface AppState{
+interface AppState {
     modal: Modals;
     loading: boolean;
     previousDate: DateTime;
@@ -20,33 +19,39 @@ interface AppState{
 
 const useStore = create<AppState>()(
     subscribeWithSelector((set) => ({
-        currentDate: DateTime.fromISO(DateTime.utc().toISODate()),
+        currentDate: DateTime.fromISO(DateTime.utc().toISODate()!),
         loading: false,
-        maxDate: DateTime.fromISO(DateTime.utc().toISODate()),
+        maxDate: DateTime.fromISO(DateTime.utc().toISODate()!),
         minDate: DateTime.utc(1995, 6, 16),
         modal: undefined,
-        nextDate: DateTime.fromISO(DateTime.utc().toISODate()).plus({ day: 1 }),
-        previousDate: DateTime.fromISO(DateTime.utc().toISODate()).minus({ day: 1 }),
+        nextDate: DateTime.fromISO(DateTime.utc().toISODate()!).plus({
+            day: 1,
+        }),
+        previousDate: DateTime.fromISO(DateTime.utc().toISODate()!).minus({
+            day: 1,
+        }),
         setDate: (date: Date): void => {
-            const currentDate = DateTime.fromISO(DateTime.fromJSDate(date).toISODate());
+            const currentDate = DateTime.fromISO(
+                DateTime.fromJSDate(date).toISODate()!
+            );
             const nextDate = currentDate.plus({ day: 1 });
             const previousDate = currentDate.minus({ day: 1 });
             set(() => ({
                 currentDate,
                 nextDate,
-                previousDate
+                previousDate,
             }));
         },
         setLoading: (loading): void => {
             set(() => ({
-                loading
+                loading,
             }));
         },
         setModal: (modal: Modals): void => {
             set(() => ({
-                modal
+                modal,
             }));
-        }
+        },
     }))
 );
 
