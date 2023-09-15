52ab07229adb9d7b495feed828a6a5932d1fb1c1
Bradley Love <brad@bradlove.co.za>
2023-09-15 15:52:00 +0200

    Update index.tsx

--- components/calendar/index.tsx
+++ components/calendar/index.tsx
@@ -1,4 +1,3 @@
-
 import { DateTime } from "luxon";
 import React, { useCallback } from "react";
 import Calander from "react-calendar";
@@ -10,52 +9,41 @@ import {
     ChevronLeft,
     ChevronRight,
     DoubleChevronLeft,
-    DoubleChevronRight
+    DoubleChevronRight,
 } from "../../icons";
 
 import style from "./index.module.css";
 
-
 export const Calendar: React.ComponentType = () => {
-
     const router = useRouter();
-    const [
-        currentDate,
-        minDate,
-        maxDate
-    ] = useStore((state) => [
-        state.currentDate,
-        state.minDate,
-        state.maxDate
-    ], shallow);
+    const [currentDate, minDate, maxDate] = useStore(
+        (state) => [state.currentDate, state.minDate, state.maxDate],
+        shallow
+    );
     const setModal = useStore((state) => state.setModal);
     const setCurrentDate = useStore((state) => state.setDate);
 
     const handleOnClick = useCallback((date: Date) => {
-
         const pathDate = DateTime.fromJSDate(date).toISODate();
 
         setModal(undefined);
 
         setCurrentDate(date);
-        router.push(`/${ pathDate }`);
-
+        router.push(`/${pathDate}`);
     }, []);
 
     return (
         <Calander
-            className={ style.calendar }
+            className={style.calendar}
             locale="EN"
-            maxDate={ DateTime.fromISO(maxDate.toISODate()).toJSDate() }
-            minDate={ minDate.toJSDate() }
-            next2Label={ <DoubleChevronRight /> }
-            nextLabel={ <ChevronRight /> }
-            onClickDay={ handleOnClick }
-            prev2Label={ <DoubleChevronLeft /> }
-            prevLabel={ <ChevronLeft /> }
-            value={ currentDate.toJSDate() }
+            maxDate={DateTime.fromISO(maxDate.toISODate()!).toJSDate()}
+            minDate={minDate.toJSDate()}
+            next2Label={<DoubleChevronRight />}
+            nextLabel={<ChevronRight />}
+            onClickDay={handleOnClick}
+            prev2Label={<DoubleChevronLeft />}
+            prevLabel={<ChevronLeft />}
+            value={currentDate.toJSDate()}
         />
     );
-
 };
-
